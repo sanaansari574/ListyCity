ced804350e634bd1c2c5d53adce5e23a217505e5
sanaansari574 <sanajamal2004@gmail.com>
2026-03-05 15:55:43 +0500

    ListyCity updated with Lab 6 tasks - tests fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,16 +31,22 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    testOptions {
+        unitTests.all {
+            it.useJUnitPlatform() // This enables JUnit 5 discovery
+        }
+    }
 }
 
 dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    implementation(files("C:\\Users\\sanaj\\AppData\\Local\\Android\\Sdk\\platforms\\android-36\\android.jar"))
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.0.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/test/java/com/example/listycity/ExampleUnitTest.java
+++ app/src/test/java/com/example/listycity/ExampleUnitTest.java
@@ -1,17 +1,18 @@
 package com.example.listycity;
 
-import org.junit.Test;
+// Use the JUnit 5 (Jupiter) package
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+// Use the JUnit 5 assertions
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
+
         assertEquals(4, 2 + 2);
     }
 }
